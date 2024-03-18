151a4a474ff87a55c04d961129e73c58763b9cce
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-03-18 10:46:23 +0530

    add : react client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query MainQueryTodos {
+    getTodosFromServer {
+      id
+      completed
+      title
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(query);
+
+  console.log({
+    loading,
+    error,
+    data,
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos</h1>
+
+      <ul>
+        {data.getTodosFromServer.map((todo) => (
+          <li key={todo.id}>
+            {todo.title} - {todo.user.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
